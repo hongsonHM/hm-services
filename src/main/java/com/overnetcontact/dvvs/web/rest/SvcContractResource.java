d747a3065314bb1634ed12122646bf4622eabdcc
vinh.nguyen <vinh.nguyen@ezcloud.vn>
2021-08-09 20:09:03 +0700

    fix missing data

--- src/main/java/com/overnetcontact/dvvs/web/rest/SvcContractResource.java
+++ src/main/java/com/overnetcontact/dvvs/web/rest/SvcContractResource.java
@@ -1,6 +1,7 @@
 package com.overnetcontact.dvvs.web.rest;
 
 import com.overnetcontact.dvvs.repository.SvcContractRepository;
+import com.overnetcontact.dvvs.service.ExcelHelper;
 import com.overnetcontact.dvvs.service.SvcContractQueryService;
 import com.overnetcontact.dvvs.service.SvcContractService;
 import com.overnetcontact.dvvs.service.criteria.SvcContractCriteria;
@@ -8,6 +9,7 @@ import com.overnetcontact.dvvs.service.dto.SvcContractDTO;
 import com.overnetcontact.dvvs.web.rest.errors.BadRequestAlertException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -19,9 +21,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import tech.jhipster.web.util.HeaderUtil;
 import tech.jhipster.web.util.PaginationUtil;
@@ -75,6 +87,27 @@ public class SvcContractResource {
             .created(new URI("/api/svc-contracts/" + result.getId()))
             .headers(HeaderUtil.createEntityCreationAlert(applicationName, false, ENTITY_NAME, result.getId().toString()))
             .body(result);
+    }
+
+    /**
+     * {@code POST  /svc-contracts} : Create a new svcContract.
+     *
+     * @param file the svcContract In excel to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new svcContractDTO, or with status {@code 400 (Bad Request)} if the svcContract has already an ID.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PostMapping(value = "/svc-contracts/excel", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> importSvcContracts(@RequestPart("file") MultipartFile file) throws URISyntaxException {
+        log.debug("REST request to save SvcContract by CSV");
+        if (!ExcelHelper.hasExcelFormat(file)) {
+            throw new BadRequestAlertException("Invalid excel type", ENTITY_NAME, "File format not support!");
+        }
+
+        Collection<SvcContractDTO> result = svcContractService.saveByExcel(file);
+        return ResponseEntity
+            .noContent()
+            .headers(HeaderUtil.createAlert(applicationName, "Excel imported!", String.valueOf(result.size())))
+            .build();
     }
 
     /**
