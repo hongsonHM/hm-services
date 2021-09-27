79fd6a12aea3aad0967a1eb32d271dd2b6980acb
Tùng Ngô <minhtungngo97@gmail.com>
2021-09-27 21:47:44 +0700

    coding preview supplies

--- src/main/java/com/overnetcontact/dvvs/repository/SvcSpendTaskRepository.java
+++ src/main/java/com/overnetcontact/dvvs/repository/SvcSpendTaskRepository.java
@@ -1,7 +1,9 @@
 package com.overnetcontact.dvvs.repository;
 
 import com.overnetcontact.dvvs.domain.SvcSpendTask;
+import java.util.List;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,4 +11,13 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface SvcSpendTaskRepository extends JpaRepository<SvcSpendTask, Long>, JpaSpecificationExecutor<SvcSpendTask> {}
+public interface SvcSpendTaskRepository extends JpaRepository<SvcSpendTask, Long>, JpaSpecificationExecutor<SvcSpendTask> {
+    @Query(
+        value = "SELECT distinct cs.name, cs.unit, cs.effort from svc_spend_task as sst " +
+        "inner join rel_core_task__core_supplies as rctcs on sst.core_task_id = rctcs.core_task_id " +
+        "inner join core_supplies as cs on rctcs.core_supplies_id = cs.id " +
+        "where sst.core_task_id in :ids",
+        nativeQuery = true
+    )
+    List<Object> findByIds(@Param("ids") List<Long> ids);
+}
